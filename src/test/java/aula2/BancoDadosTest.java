bc7ae4de2997483f5415b9a4fc8e5af58c9691ef
Marcelo Barbosa Soares <marcelo.soares@cwi.com.br>
2021-03-02 21:30:14 -0300

    cricao de pasta de teste

---
+++ src/test/java/aula2/BancoDadosTest.java
@@ -0,0 +1,19 @@
+package aula2;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BancoDadosTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
